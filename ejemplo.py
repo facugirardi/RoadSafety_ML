6b579313bb175afc067ccf6d314fab14c1c71c9d
facugirardi <facugirardi22@gmail.com>
2023-05-29 20:56:57 -0300

    arreglo

--- ejemplo.py
+++ ejemplo.py
@@ -14,30 +14,20 @@ x = data['data']
 # Variable a predecir (prediccion)
 y = data['target']
 
-# Division de datos
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.40)
-
 # Crear modelo
 model = XGBClassifier()
 
-# Definir parametros
-parameters = {}
-
-gs = GridSearchCV(model, parameters)
-gs.fit(X_test, y_test, verbose=False)
-
-# Seleccionar los mejores parametros
-model.fit(X_train, y_train)
+model.fit(x, y)
 
 """ Ya se puede empezar a realizar predicciones
  En este caso predict_proba arroja un vector en el que se muestra la probabilidad del cancer benigno y el maligno. 
 """
 
-pred_prob = model.predict_proba(X_train)
+pred_prob = model.predict_proba(x)
 print(pred_prob)
 
 # Explicar el modelo utilizando SHAP
 explainer_shap = shap.TreeExplainer(model)
 
-shap_values = explainer_shap.shap_values(X_test)
-shap.summary_plot(shap_values, X_test, feature_names=data['feature_names'])
+shap_values = explainer_shap.shap_values(x)
+shap.summary_plot(shap_values, x, feature_names=data['feature_names'])
